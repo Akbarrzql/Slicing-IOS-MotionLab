2752bc80ec8caff1e07681ee6713c1ef6a42088c
Akbar Rizqullah Putra Susanto <akbarrizqullah228@gmail.com>
2024-12-10 19:04:21 +0700

    Initial Commit

---
+++ Slicing-iOS-UI/Slicing_iOS_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slicing_iOS_UIApp.swift
+//  Slicing-iOS-UI
+//
+//  Created by Akbar Rizqullah on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Slicing_iOS_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Slicing-iOS-UITests/Slicing_iOS_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Slicing_iOS_UITests.swift
+//  Slicing-iOS-UITests
+//
+//  Created by Akbar Rizqullah on 10/12/24.
+//
+
+import Testing
+@testable import Slicing_iOS_UI
+
+struct Slicing_iOS_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
